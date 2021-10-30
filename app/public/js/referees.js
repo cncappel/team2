c4455f779b2a9b2587e604a3f351b19bb67b9aa9
cncappel <cncappel@iu.edu>
2021-10-30 15:47:50 -0400

    referee details table added

---
+++ app/public/js/referees.js
@@ -0,0 +1,32 @@
+const refereesApp = {
+    data() {
+      return {
+        referees: [],
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date).format('DD MMM YYYY')
+        }
+    },
+    methods: {
+        fetchRefereeData() {
+            fetch('/api/referees/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referees = responseJson;
+                console.log('pulled users data');
+            })
+            .catch( (err) => {
+                console.error(err);
+                console.log('users data not pulling');
+            })
+        },
+    },
+    created() {
+        this.fetchRefereeData();
+    }
+  }
+  
+Vue.createApp(refereesApp).mount('#refereesApp');
