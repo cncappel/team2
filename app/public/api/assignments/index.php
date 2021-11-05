47c797f361efab00b4df1b8b1613080d06f53675
aatalib <aatalib@iu.edu>
2021-11-05 13:45:07 -0400

    Working Outer Join api

--- app/public/api/assignments/index.php
+++ app/public/api/assignments/index.php
@@ -11,7 +11,7 @@ $vars = [];
 
 if (isset($_GET['referee'])) {
   // This is an example of a parameterized query
-  $sql = 'SELECT * FROM AssignmentStatus WHERE refID = ?';
+  $sql = 'SELECT * FROM AssignmentStatus WHERE id = ?';
   $vars = [ $_GET['referee'] ];
 }
 
