cba6bfd67ec8c4933a44620af9da10ba7330f555
cncappel <cncappel@iu.edu>
2021-11-06 17:42:01 -0400

    table updates

--- app/public/js/games.js
+++ app/public/js/games.js
@@ -1,7 +1,9 @@
 const gameApp = {
     data() {
       return {
-        games: []
+        games: [],
+        selectedGame: null,
+        gameForm: {}
       }
     },
     computed: {},
@@ -18,6 +20,75 @@ const gameApp = {
                 console.error(err);
                 console.log('game data not pulling');
             })
+        },
+        postNewGame(evt) {
+          fetch('api/games/create.php', {
+              method:'POST',
+              body: JSON.stringify(this.gameForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.games = json;
+              
+              // reset the form
+              this.resetGameForm();
+            });
+        },
+        postDeleteGame(o) {
+          if (!confirm("Are you sure you want to delete the offer from "+o.field+"?")) {
+              return;
+          }
+          
+          fetch('api/games/delete.php', {
+              method:'POST',
+              body: JSON.stringify(o),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.games = json;
+              
+              this.resetGameForm();
+            });
+        },
+        postEditGame(evt) {
+          this.gameForm.gameID = this.selectedGame.gameID;       
+          
+          console.log("Updating!", this.gameForm);
+  
+          fetch('api/games/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.gameForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.games = json;
+              console.log("im stringified")
+              
+              this.resetGameForm();
+            });
+        },
+      resetGameForm() {
+        this.selectedGame = null;
+        this.gameForm = {};
+      },
+      selectGame(r) {
+          this.selectedGame = r;
+          this.gameForm = Object.assign({}, this.selectedGame);
         },
     },
     created() {
